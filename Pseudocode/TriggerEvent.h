0d7453ebc3d20cec2a503f6e87b4b1cbc28f2ca8
irgiastadhani334 <irgiastadhani420@gmail.com>
2026-06-16 20:48:35 +0700

    Create TriggerEvent.h

---
+++ Pseudocode/TriggerEvent.h
@@ -0,0 +1,16 @@
+#ifndef TRIGGER_EVENT_H
+#define TRIGGER_EVENT_H
+
+enum class TriggerEvent {
+    EnterShop,
+    EnterBlind,
+    BuyCard
+};
+
+enum class TagType {
+    FreeJoker,
+    DoubleMoney,
+    MegaBlindBuff
+};
+
+#endif
